d260434bb12ca0877a45ca68b88bd8beb2e9b671
Shariful134 <shariful32213@gmail.com>
2025-03-01 01:28:49 +0600

    context create done

--- src/app/(WithCommonLayout)/page.tsx
+++ src/app/(WithCommonLayout)/page.tsx
@@ -1,8 +1,9 @@
-import { getCurrentUser } from "@/services/authService";
+"use client";
+import { useUser } from "@/context/UserContext";
 
-const HomePage = async () => {
-  const currentuserData = await getCurrentUser();
-  console.log(currentuserData);
+const HomePage = () => {
+  const user = useUser();
+  console.log(user);
   return (
     <div>
       <h2 className="text-3xl">This is home page</h2>
